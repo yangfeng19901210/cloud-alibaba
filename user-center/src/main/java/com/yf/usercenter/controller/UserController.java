6d1cb0679d609dfb867ceb6058b51e444cb139b0
“yangfeng” <18334723118@163.com>
2022-03-21 11:33:39 +0800

    添加并提交非空验证方法

--- user-center/src/main/java/com/yf/usercenter/controller/UserController.java
+++ user-center/src/main/java/com/yf/usercenter/controller/UserController.java
@@ -3,9 +3,13 @@ package com.yf.usercenter.controller;
 
 import com.yf.usercenter.entity.User;
 import com.yf.usercenter.service.UserService;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
@@ -28,6 +32,15 @@ public class UserController {
   public User getUserById(@PathVariable Integer id){
     return userService.getById(id);
   }
+
+  @PostMapping
+  public String saveUser(@RequestBody @Valid User user, BindingResult results){
+    if (results.hasErrors())
+      return results.getFieldError().getDefaultMessage();
+    userService.save(user);
+    return "ok";
+  }
+
 
 }
 
