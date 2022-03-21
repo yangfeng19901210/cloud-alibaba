502a7a3610e9ca7779de05276a288cc897e7c998
“yangfeng” <18334723118@163.com>
2022-03-21 17:02:08 +0800

    添加自定义代码生成

---
+++ user-center/src/main/java/com/yf/usercenter/utils/CustomGenerator.java
@@ -0,0 +1,70 @@
+package com.yf.usercenter.utils;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.StringReader;
+
+/**
+ * @author yangfeng
+ * @version 1.0
+ * @date 2022-03-21 16:59
+ */
+public class CustomGenerator {
+  /***
+   * @author yf
+   * @date 2022-03-21 17:00:01
+   * @param res 需要生成的字符串
+   * @param filePath 生辰文件所在的路径
+   * @return
+   **/
+  public static boolean string2File(String res, String filePath) {
+    boolean flag = true;
+    BufferedReader bufferedReader = null;
+    BufferedWriter bufferedWriter = null;
+    try {
+      File distFile = new File(filePath);
+      if (!distFile.getParentFile().exists()) distFile.getParentFile().mkdirs();
+      bufferedReader = new BufferedReader(new StringReader(res));
+      bufferedWriter = new BufferedWriter(new FileWriter(distFile));
+      char buf[] = new char[1024];         //字符缓冲区
+      int len;
+      while ((len = bufferedReader.read(buf)) != -1) {
+        bufferedWriter.write(buf, 0, len);
+      }
+      bufferedWriter.flush();
+      bufferedReader.close();
+      bufferedWriter.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+      flag = false;
+      return flag;
+    } finally {
+      if (bufferedReader != null) {
+        try {
+          bufferedReader.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+    return flag;
+  }
+
+  public static void main(String[] args) {
+    String path = "D:/codeHome/entity";
+    File file = new File(path);
+    if (!file.exists()) {
+      file.mkdirs();
+    }
+    StringBuilder sb = new StringBuilder();
+    sb.append("com.yf.usercenter\n");
+    sb.append("public class User {\n");
+    sb.append("   private String name;\n");
+    sb.append("}");
+    string2File(sb.toString(),"D:/codeHome/entity/User.java");
+  }
+
+}
