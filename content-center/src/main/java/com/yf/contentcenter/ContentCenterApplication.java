791d83ccc1a7ad376666ec5d580ad9fef20867d4
“yangfeng” <18334723118@163.com>
2022-03-23 17:12:05 +0800

    添加静态代码块测试

--- content-center/src/main/java/com/yf/contentcenter/ContentCenterApplication.java
+++ content-center/src/main/java/com/yf/contentcenter/ContentCenterApplication.java
@@ -5,6 +5,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -26,8 +27,11 @@ public class ContentCenterApplication {
   public PaginationInterceptor paginationInterceptor() {
     return new PaginationInterceptor();
   }
-
+  /***
+   * @LoadBalanced  能让客户端在请求时拥有负载均衡能力
+   **/
   @Bean
+  @LoadBalanced
   public RestTemplate restTemplate(){
     return new RestTemplate();
   }
